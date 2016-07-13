b009714a9ab637225b7deec63be2cd442b25a826
German <german_442@hotmail.com>
2016-07-13 12:18:59 -0700

    Curso Tr

--- odoo_academy/academy.py
+++ odoo_academy/academy.py
@@ -9,5 +9,17 @@ class academy_student(models.Model):
     last_name = fields.Char('Apellido', size=128)
     photo = fields.Binary('Fotografia')
     create_date = fields.Datetime('Fecha Creacion', readonly=True)
+    notes =  fields.Html('Comentarios')
+    active = fields.Boolean('Activo')
+    state = fields.Selection([('draft','Documento Borrador'),
+                              ('progress','Progeso'),
+                              ('done','Egresado'),], 'Estado')
+
 
     _order = 'name'
+
+    _defaults = {  
+        'active': True,
+        'state': 'draft',
+        
+        }
